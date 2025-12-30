a97c181b11c215f894cb28dd3291cc221cfc2965
PrinceNwaimo <nwaimoprince12@gmail.com>
2025-12-30 19:34:30 +0100

    final commit for dev

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 import path from 'path';
-
 import { miaodaDevPlugin } from 'miaoda-sc-plugin';
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/HGPACI-Website/', // 👈 REQUIRED for GitHub Pages
   plugins: [
     react(),
     svgr({
